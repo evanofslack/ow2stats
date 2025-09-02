9d697e295da657a3a14eb3c7e2e9e89dfe8e679a
evan slack <evan.slack@outlook.com>
2025-09-01 20:25:02 -0400

    logger for client

--- scrape/src/client.py
+++ scrape/src/client.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 from typing import List
 from dataclasses import asdict
@@ -8,28 +9,33 @@ from .models import HeroStatsUpload
 class BackendClient:
     """Client for interacting with the OW2Stats backend API."""
 
-    def __init__(self, backend_url: str):
+    def __init__(
+        self,
+        logger: logging.Logger,
+        backend_url: str,
+    ):
         if not backend_url:
             raise ValueError("backend_url must be provided.")
         self.backend_url = backend_url.rstrip("/")
         self.batch_upload_url = f"{self.backend_url}/api/v1/heroes/batch"
+        self.logger = logger
 
     def upload_stats(self, stats: List[HeroStatsUpload]):
         """Uploads a list of hero statistics to the backend."""
         if not stats:
-            print("No stats to upload")
+            self.logger.debug("No stats to upload")
             return
 
+        # Convert list of dataclasses to a list of dictionaries
         payload = [asdict(stat) for stat in stats]
 
         try:
-            # Convert list of dataclasses to a list of dictionaries
             response = requests.post(self.batch_upload_url, json=payload, timeout=15)
             response.raise_for_status()
-            print(
-                f"Successfully uploaded {len(stats)} hero stats, count={len(stats)}, url={self.batch_upload_url}"
+            self.logger.debug(
+                f"Successfully uploaded hero stats, count={len(stats)}, url={self.batch_upload_url}"
             )
         except requests.RequestException as e:
-            print(
+            self.logger.warn(
                 f"An error occurred while uploading stats to the backend, err={e}, url={self.batch_upload_url}, stat={stats[0]}"
             )
